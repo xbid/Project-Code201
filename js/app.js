78df45bc57a790a72a2b9201215ab40753cba6e2
xbid <abdzubaid@gmail.com>
2021-02-09 04:01:16 +0200

    finished but i will come back i think ihave a bug in my code

--- js/app.js
+++ js/app.js
@@ -1,2 +1,97 @@
-`use strict`;
+'use strict';
 
+
+let userName = prompt('\t   Who are you..?\n\nPlease tell us your name..!\n ');
+console.log(userName);
+
+function greeting(message){
+  alert(message);
+}
+greeting('\n\tWelcome to my WebPage..' + userName.toUpperCase() + ' \n\t\t\tIam Abdallah\n\t\tI used to work as a chef\n\t\t\tIam from irbid\nI would like it if you played this guessing game');
+
+
+let userConfirm = prompt('Do You Like Quzzies? Answer with yes/no');
+
+console.log(userConfirm.toLowerCase());
+
+userConfirm = userConfirm.toLowerCase();
+
+if (userConfirm ==='yes' || userConfirm ==='y'){
+  console.log('I think you will have fun ..!');
+} else if(userConfirm ==='no' || userConfirm ==='n'){
+  console.log('Come On ..!, Just try it\n\tIt will be fun');
+}
+console.log(userConfirm);
+
+let favFood = prompt ('Do i love Mansaf?');
+
+console.log(favFood.toLowerCase());
+
+favFood = favFood.toLowerCase();
+
+if (favFood ==='yes' || favFood ==='y'){
+  console.log('Bingo Thats right');
+} else if(favFood ==='no' || favFood ==='n'){
+  console.log('Wrong..?\n\tCome on i said that before');
+}else{
+  console.log(favFood);
+}
+
+let petType = prompt ('Am i a cat person');
+
+console.log(petType.toLowerCase());
+
+petType = petType.toLowerCase();
+
+if (petType ==='yes' || petType ==='y'){
+  console.log('Yes bravo you nailed it ');
+} else if(petType ==='no' || petType ==='n'){
+  console.log('no wrong but if there is any comfort to you\n i like dogs & parrots too but not as the fluffy pets');
+}else{
+  console.log(petType);
+}
+
+let bestStudent = prompt ('Do you think iam a good student?');
+
+console.log(bestStudent.toLowerCase());
+
+bestStudent = bestStudent.toLowerCase();
+
+if (bestStudent ==='yes' || bestStudent ==='y'){
+  console.log('Really!!,.. Thanks a lot for your honest answer infact i feel like i am so thank you a lot for your supporting all of us');
+} else if(bestStudent ==='no' || bestStudent ==='n'){
+  console.log('Nooo, come on why you answered you should believe in me');
+}else{
+  console.log(bestStudent);
+}
+
+
+
+let homeJob = prompt ('Because iam a chef as i said before, Do you think i cook in home?');
+
+console.log(homeJob.toLowerCase());
+
+homeJob = homeJob.toLowerCase();
+
+if (homeJob ==='yes' || homeJob ==='y'){
+  console.log('you are right again oh omg i feel like you really know me');
+} else if(homeJob ==='no' || homeJob ==='n'){
+  console.log('well believe it or not infact i do usually... lol');
+}else{
+  console.log(homeJob);
+}
+
+let favTime = prompt ('what time do i like to work on tasks? is it night?');
+
+console.log(favTime.toLowerCase());
+
+favTime = favTime.toLowerCase();
+
+if (favTime ==='yes' || favTime ==='y'){
+  console.log('yes and now i started to believe that you know me very well');
+} else if(favTime ==='no' || favTime ==='n'){
+  console.log('wrong you dont know anything about me infact i have a quote about that on my whats app status');
+}else{
+  console.log(favTime);
+}
+alert('welcome again' + userName.toUpperCase() + 'I hope you enjoyed the quizes see told you it will be fun' );
